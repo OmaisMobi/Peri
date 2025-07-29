28ccbe22ab63d48174e52bf6191cf21633121747
OmaisMobi <omais.mobipixels@gmail.com>
2025-07-29 15:41:49 +0500

    bootstrap

---
+++ bootstrap/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\FilamentPaymentsServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\ClientPanelProvider::class,
+    App\Providers\Filament\EmployeePanelProvider::class,
+];
